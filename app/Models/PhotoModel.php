5475593a22248cab71477c5fb183ba33c41b0eab
Adina Zhantykeyeva <adinazhantykeyeva@Adinas-MacBook-Air.local>
2024-10-25 11:18:05 +0200

    models created

---
+++ app/Models/PhotoModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhotoModel extends Model
+{
+    use HasFactory;
+    public function animal()
+    {
+        return $this->hasOne(AnimalModel::class);
+    }
+}
